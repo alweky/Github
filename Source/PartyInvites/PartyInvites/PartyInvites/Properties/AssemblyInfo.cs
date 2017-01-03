f04008c46e86af78941b0c07a64b6d7216e05901
alwekyphone <alwekyphone@gmail.com>
2017-01-02 21:04:12 -0500

    Getting Started

--- Source/PartyInvites/PartyInvites/PartyInvites/Properties/AssemblyInfo.cs
+++ Source/PartyInvites/PartyInvites/PartyInvites/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d38cd58b-abfb-43fc-8d5d-9e8b5bdeb926")]
+[assembly: Guid("a1ddc0eb-d263-45c8-9121-48be340cf378")]
 
 // Version information for an assembly consists of the following four values:
 //
