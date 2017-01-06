3a12c24ae9ec2ba71ee23d0c6b940d94b4d7f44b
alwekyphone <alwekyphone@gmail.com>
2017-01-05 19:48:54 -0500

    Adding a New Field

---
+++ Source/MvcMovie/MvcMovie/Migrations/201701060036407_Rating.cs
@@ -0,0 +1,19 @@
+namespace MvcMovie.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Rating : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Movie", "Rating", c => c.String());
+            
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Movie", "Rating");
+        }
+    }
+}
